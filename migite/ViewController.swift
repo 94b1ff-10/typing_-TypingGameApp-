cf9fb63db49953eae5965bd4325edcdd775afc4a
No.10 <maruten007@gmail.com>
2022-11-28 11:51:18 +0900

    Initial Commit

---
+++ migite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  migite
+//
+//  Created by TEN MATSUMOTO on 28/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
